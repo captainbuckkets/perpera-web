6a69ef727ebd0d764a59e62523784893b63ff404
Jack <jack.day.27@gmail.com>
2021-11-01 20:48:29 -0600

    Adding ledger status

--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -1,20 +1,40 @@
 import * as React from "react";
 import Loader from "src/components/Loader/Loader";
 import "./Status.css";
+import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
 
 interface IState {
   apiStatus: boolean;
+  ledgerStatus: boolean;
   isLoading: boolean;
 }
 
 class Status extends React.Component<{}, IState> {
   constructor(props: any) {
     super(props);
+
+    this.initLedger = this.initLedger.bind(this);
+
     this.state = {
       apiStatus: false,
+      ledgerStatus: false,
       isLoading: false
     };
   }
+
+  public initLedger(e: any) {
+    try {
+      e.preventDefault();
+      console.log("initing ledger")
+      TransportWebUSB.create().then(transport => {
+        console.log(transport)
+        this.setState({
+          ledgerStatus: true
+        });
+      })
+    } catch (error) { console.log(error) }
+  }
+
   public componentDidMount() {
     // simplest solution : used getaddress API with old address
     fetch("https://blockbook.peercoin.net/api")
@@ -45,6 +65,17 @@ class Status extends React.Component<{}, IState> {
         {!this.state.apiStatus && (
           <div className="status-btn">
             <img src="img/status_off.svg" alt="disconnected" />
+          </div>
+        )}
+
+        {this.state.ledgerStatus && (
+          <div className="status-btn status-btn-ledger">
+            <img id="ledgerImage" src="img/ledger-connected.png" alt="disconnected" />
+          </div>
+        )}
+        {!this.state.ledgerStatus && (
+          <div className="status-btn status-btn-ledger" onClick={this.initLedger} >
+            <img id="ledgerImage" src="img/ledger.png" alt="disconnected" />
           </div>
         )}
       </div>
